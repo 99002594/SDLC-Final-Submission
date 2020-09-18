b5f5f0cdde3c90b004e0177984f432153b68251c
99002594 <71481300+99002594@users.noreply.github.com>
2020-09-18 17:40:40 +0530

    Update operations.c

--- Calculator/src/operations.c
+++ Calculator/src/operations.c
@@ -23,4 +23,55 @@ int divide(int operand1, int operand2)
         return operand1 / operand2;
 }
 
+float power(float operand1, float operand2)
+{
+    if(operand1==0)
+        return 0;
+    else 
+        return pow(operand1,operand2);
+}
 
+
+
+float logarithm(float operand1)
+{
+    if(operand1<=0)
+        return 0;
+    else
+        return log10(operand1);
+        
+}
+
+
+float natural_logarithm(float operand1)
+{
+    if(operand1<=0)
+        return 0;
+    else
+        return log(operand1);
+        
+}
+
+
+float sine(float operand1)
+{
+    return sin(operand1);
+}
+
+
+float cosine(float operand1)
+{
+    return cos(operand1);
+}
+
+
+float tangent(float operand1)
+{
+    return tan(operand1);
+}
+
+
+float cotangent(float operand1)
+{
+    return 1/tan(operand1);
+}
