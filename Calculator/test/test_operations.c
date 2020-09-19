173a79b8d325101a9faf6cb2eff3517ff7f6f68b
99002594 <71481300+99002594@users.noreply.github.com>
2020-09-19 06:48:55 +0530

    Update test_operations.c

--- Calculator/test/test_operations.c
+++ Calculator/test/test_operations.c
@@ -10,6 +10,13 @@ void test_add(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
+void test_power(void);
+void test_logarithm(void);
+void test_natural_logarithm(void);
+void test_sine(void);
+void test_cosine(void);
+void test_tangent(void);
+void test_cotangent(void);
 
 /* Start of the application test */
 int main() {
@@ -26,6 +33,13 @@ int main() {
   CU_add_test(suite, "subtract", test_subtract);
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
+  CU_add_test(suite, "power", test_power);
+  CU_add_test(suite, "logarithm", test_logarithm);
+  CU_add_test(suite, "natural_logarithm", test_natural_logarithm);
+  CU_add_test(suite, "sine", test_sine);
+  CU_add_test(suite, "cosine", test_cosine);
+  CU_add_test(suite, "tangent", test_tangent);
+  CU_add_test(suite, "cotangent", test_cotangent);
 
 
 /* Note: Do not edit START*/
@@ -68,4 +82,53 @@ void test_divide(void) {
   
   /* Dummy fail*/
   CU_ASSERT(3 == divide(2, 2));
+}
+void test_power(void) {
+  CU_ASSERT(1 == power(1, 0));
+  CU_ASSERT(0==power(0,0);
+  CU_ASSERT(256==power(4,4);
+  /* Dummy fail*/
+  CU_ASSERT(3 == power(2, 2));
+}
+void test_logarithm(void) {
+  CU_ASSERT(0 == logarithm(1));
+  CU_ASSERT(0 == logarithm(0));
+  CU_ASSERT(0 == logarithm(-1));
+  CU_ASSERT(2 == logarithm(100));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == logarithm(2));
+}
+void test_natural_logarithm(void) {
+  CU_ASSERT(0 == natural_logarithm(1));
+  CU_ASSERT(0 == natural_logarithm(0));
+  CU_ASSERT(0 == natural_logarithm(-1));
+  CU_ASSERT(4.605 == natural_logarithm(100));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == natural_logarithm(2));
+}
+void test_sine(void) {
+  CU_ASSERT(1.5707 == sine(1));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == sine(2));
+}
+void test_cosine(void) {
+  CU_ASSERT(1 == cosine(0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == cosine(2));
+}
+void test_tangent(void) {
+  CU_ASSERT(0.7853 == tangent(1));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == tangent(2));
+}
+void test_cotangent(void) {
+  CU_ASSERT(1.2732 == cotangent(1));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == cotangent(2));
 }
