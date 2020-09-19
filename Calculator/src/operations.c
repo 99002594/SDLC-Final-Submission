90d31221f33844c365c73b47619f23b71d3e1008
99002594 <71481300+99002594@users.noreply.github.com>
2020-09-19 09:42:14 +0530

    Update operations.c

--- Calculator/src/operations.c
+++ Calculator/src/operations.c
@@ -77,3 +77,61 @@ float cotangent(float operand1)
     return 1/tan(operand1);
 }
 
+float square_root(float operand1)
+{
+    if(operand1<=0)
+        return 0;
+    else
+        return sqrt(operand1);
+}
+
+float cube_root(float operand1)
+{
+    if(operand1<=0)
+        return 0;
+    else
+        return pow(operand1,3);
+}
+
+float Higher_root(float operand1, float operand2)
+{
+    if(operand1==0)
+        return 0;
+    else
+        return pow(operand1,1/operand2);
+}
+
+float inverse(float operand1)
+{
+    if(operand1<=0)
+        return 0;
+    else
+        return 1/operand1;
+}
+ 
+float deg_to_rad(float operand1)
+{
+    float radians=operand1*(22/7)/180;
+    return radians;
+}
+
+float rad_to_deg(float operand1)
+{
+    float degree=operand1*(180/(22/7));
+    return degree;
+}
+
+float ceil_value(float operand1)
+{
+    return ceil(operand1);
+}
+
+float floor_value(float operand1)
+{
+    return floor(operand1);
+}
+
+float absolute_value(float operand1)
+{
+    return abs(operand1);
+}
