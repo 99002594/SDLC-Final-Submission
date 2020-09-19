a74d5048b188bb693ceeeb474bbe3426c646c80f
99002594 <71481300+99002594@users.noreply.github.com>
2020-09-19 09:42:04 +0530

    Update operations.h

--- Calculator/inc/operations.h
+++ Calculator/inc/operations.h
@@ -66,7 +66,7 @@ float logarithm(float operand1);
 * logarithm of operand1 and returns the result 
 * @param[in] operand1  
 * @return float value of the natural logarithm of operand1
-* @note returns 0 for logarithm of 0 error
+* @note returns 0 for natural logarithm of 0 error
 */
 float natural_logarithm(float operand1);
 
@@ -74,29 +74,95 @@ float natural_logarithm(float operand1);
 * sine of operand1 and returns the result 
 * @param[in] operand1  
 * @return float value of the sine of operand1
-* @note returns 0 for logarithm of 0 error
 */
 float sine(float operand1);
 
 /**
 * cosine of operand1 and returns the result 
 * @param[in] operand1  
-* @return float value of the sine of operand1
+* @return float value of the cosine of operand1
 */
 float cosine(float operand1);
 
 /**
 * tangent of operand1 and returns the result 
 * @param[in] operand1  
-* @return float value of the sine of operand1
+* @return float value of the tan of operand1
 */
 float tangent(float operand1);
 
 /**
 * cotangent of operand1 and returns the result 
 * @param[in] operand1  
-* @return float value of the sine of operand1
+* @return float value of the cot of operand1
 */
 float cotangent(float operand1);
 
+/**
+* square root of operand1 and returns the result 
+* @param[in] operand1  
+* @return float value of the square root of operand1
+* @note returns 0 for square root of less than 0 
+*/
+float square_root(float operand1);
+
+/**
+* cube root of operand1 and returns the result 
+* @param[in] operand1  
+* @return float value of the square root of operand1
+* @note returns 0 for cube root of less than 0 
+*/
+float cube_root(float operand1);
+
+/**
+* roots the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return float value of the operand1 root to operand2
+* @note returns 0 for higher root of 0 error
+*/
+float Higher_root(float operand1, float operand2);
+
+/**
+* inverse of operand1 and returns the result 
+* @param[in] operand1  
+* @return float value of the inverse of operand1
+* @note returns 0 for inverse of 0 
+*/
+float inverse(float operand1);
+
+/**
+* degree to radian of operand1 and returns the result 
+* @param[in] operand1  
+* @return float value of the radian of operand1 
+*/
+float deg_to_rad(float operand1);
+
+/**
+* radian to degree of operand1 and returns the result 
+* @param[in] operand1  
+* @return float value of the degree of operand1 
+*/
+float rad_to_deg(float operand1);
+
+/**
+* ceil value of operand1 and returns the result 
+* @param[in] operand1  
+* @return ceil value of operand1 
+*/
+float ceil_value(float operand1);
+
+/**
+* floor value of operand1 and returns the result 
+* @param[in] operand1  
+* @return floor value of operand1 
+*/
+float floor_value(float operand1);
+
+/**
+* absolute value of operand1 and returns the result 
+* @param[in] operand1  
+* @return absolute value of operand1 
+*/
+float absolute_value(float operand1);
 #endif  /* #define __OPERATIONS_H__ */
